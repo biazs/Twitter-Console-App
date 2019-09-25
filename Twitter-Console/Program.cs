76fcaf2315a1147721633d0f62b98353831887f1
Bia <fabianazs@gmail.com>
2019-09-25 09:18:44 -0300

    Tweets 140 chars

--- Twitter-Console/Program.cs
+++ Twitter-Console/Program.cs
@@ -10,6 +10,28 @@ namespace Twitter_Console
     {
         static void Main(string[] args)
         {
+            string[] tweets = new string[4];
+
+            for (int i = 0; i < tweets.Length; i++)
+            {
+                Console.Write("Enter tweet {0}: ", i + 1);
+                tweets[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Tweets posted successfully.");
+            Console.WriteLine();
+
+            for (int i = 0; i < tweets.Length; i++)
+            {
+                if (tweets[i].Length > 140)
+                    Console.WriteLine("Tweet is longer than 140 chars.");
+
+                else
+                    Console.WriteLine("Tweet {0}", i + 1 + ": " + tweets[i]);
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
